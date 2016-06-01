f2b3bd6014c46990d262dfa2ec0bb76a5a2f0865
Redth <jondick@gmail.com>
2016-06-01 13:22:50 -0400

    Added firebase-common bindings Almost everything depends on this library at some level now.

---
+++ firebase-common/source/Additions/Manifest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.Content;
+
+namespace Firebase.Provider
+{
+    // <provider
+    //    android:authorities="${applicationId}.firebaseinitprovider"
+    //    android:name=".provider.FirebaseInitProvider"
+    //    android:exported="false"
+    //    android:initOrder="100" />
+    [ContentProvider (new [] { "${applicationId}.firebaseinitprovider" },
+        Name="com.google.firebase.provider.FirebaseInitProvider",
+        Exported=true, 
+        InitOrder=100)]
+    public partial class FirebaseInitProvider
+    {
+    }
+}
+
