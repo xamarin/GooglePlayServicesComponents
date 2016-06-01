d80329c97c37cf843b0b0a5ab3ed5cfc10c8842a
Redth <jondick@gmail.com>
2016-06-01 12:33:26 -0400

    Bump versions and ready build scripts for update

--- GpsConsts.cs
+++ GpsConsts.cs
@@ -3,6 +3,13 @@
 //      https://dl-ssl.google.com/android/repository/addon.xml
 
 static class __GpsConsts {
-    public const string Url  = "https://dl-ssl.google.com/android/repository/google_m2repository_r24.zip";
-    public const string Version = "8.4.0";
+    public const string Url  = "https://dl-ssl.google.com/android/repository/google_m2repository_r27.zip";
+    public const string Version = "9.0.1";
+    public const string WearVersion = "1.4.0";
+}
+
+static class __FbConsts
+{
+    public const string Url = __GpsConsts.Url;
+    public const string Version = __GpsConsts.Version;
 }
