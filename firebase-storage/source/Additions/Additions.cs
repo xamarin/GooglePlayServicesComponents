a221faabc705c130b0a2ab3d3709981b24324ba7
Redth <jondick@gmail.com>
2016-06-01 13:59:06 -0400

    Added firebase-storage and firebase-storage-common bindings

---
+++ firebase-storage/source/Additions/Additions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.Runtime;
+
+namespace Firebase.Storage
+{
+    public partial class FileDownloadTask
+    {
+        static IntPtr id_getResult;
+        protected override unsafe global::Java.Lang.Object RawResult {
+            [Register ("getResult", "()Ljava/lang/Object;", "GetGetRawResultHandler")]
+            get {
+                if (id_getResult == IntPtr.Zero)
+                    id_getResult = JNIEnv.GetMethodID (class_ref, "getResult", "()Ljava/lang/Object;");
+                try {
+                    return global::Java.Lang.Object.GetObject<global::Java.Lang.Object> (JNIEnv.CallObjectMethod  (Handle, id_getResult), JniHandleOwnership.TransferLocalRef);
+                } finally {
+                }
+            }
+        }
+    }
+
+    public partial class StreamDownloadTask
+    {
+        static IntPtr id_getResult;
+        protected override unsafe global::Java.Lang.Object RawResult {
+            [Register ("getResult", "()Ljava/lang/Object;", "GetGetRawResultHandler")]
+            get {
+                if (id_getResult == IntPtr.Zero)
+                    id_getResult = JNIEnv.GetMethodID (class_ref, "getResult", "()Ljava/lang/Object;");
+                try {
+                    return global::Java.Lang.Object.GetObject<global::Java.Lang.Object> (JNIEnv.CallObjectMethod  (Handle, id_getResult), JniHandleOwnership.TransferLocalRef);
+                } finally {
+                }
+            }
+        }
+    }
+
+    public partial class UploadTask
+    {
+        static IntPtr id_getResult;
+        protected override unsafe global::Java.Lang.Object RawResult {
+            [Register ("getResult", "()Ljava/lang/Object;", "GetGetRawResultHandler")]
+            get {
+                if (id_getResult == IntPtr.Zero)
+                    id_getResult = JNIEnv.GetMethodID (class_ref, "getResult", "()Ljava/lang/Object;");
+                try {
+                    return global::Java.Lang.Object.GetObject<global::Java.Lang.Object> (JNIEnv.CallObjectMethod  (Handle, id_getResult), JniHandleOwnership.TransferLocalRef);
+                } finally {
+                }
+            }
+        }
+    }
+}
+
