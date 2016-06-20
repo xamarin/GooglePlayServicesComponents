c6d89db977c0ac4f1dab963c4ee4c24f031d7cde
Redth <jondick@gmail.com>
2016-06-20 14:16:45 -0400

    Added async'ified version of bound method

---
+++ firebase-common/source/Additions/Additions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Android.Gms.Tasks;
+using Android.Gms.Extensions;
+
+namespace Firebase.Auth
+{
+    public partial class FirebaseUser
+    {
+        public System.Threading.Tasks.Task<IAuthResult> LinkWithCredentialAsync (AuthCredential credential)
+        {
+            return LinkWithCredential (credential).AsAsync<IAuthResult> ();
+        }
+    }
+}
