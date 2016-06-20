192c47e3d7a02ee48a144ef306e653701ece3307
Redth <jondick@gmail.com>
2016-06-20 13:59:53 -0400

    Added some async'ified versions of bound methods

---
+++ firebase-auth/source/Additions/Additions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.Gms.Extensions;
+
+namespace Firebase.Auth
+{
+    public partial class FirebaseAuth
+    {
+        public System.Threading.Tasks.Task<IAuthResult> SignInAnonymouslyAsync ()
+        {
+            return SignInAnonymously ().AsAsync<IAuthResult> ();
+        }
+
+        public System.Threading.Tasks.Task<IAuthResult> SignInWithCustomTokenAsync (string customToken)
+        {
+            return SignInWithCustomToken (customToken).AsAsync<IAuthResult> ();
+        }
+
+        public System.Threading.Tasks.Task<IAuthResult> CreateUserWithEmailAndPasswordAsync (string email, string password)
+        {
+            return CreateUserWithEmailAndPassword (email, password).AsAsync<IAuthResult> ();
+        }
+
+        public System.Threading.Tasks.Task<IAuthResult> SignInWithCredentialAsync (AuthCredential authCredential)
+        {
+            return SignInWithCredential (authCredential).AsAsync<IAuthResult> ();
+        }
+
+        public System.Threading.Tasks.Task<IAuthResult> SignInWithEmailAndPasswordAsync (string email, string password)
+        {
+            return SignInWithEmailAndPassword (email, password).AsAsync<IAuthResult> ();
+        }
+    }
+}
+
