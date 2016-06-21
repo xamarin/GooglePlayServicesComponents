00de8e2d4263c9818c7df7b1428ff40d3f197707
Redth <jondick@gmail.com>
2016-06-21 10:10:03 -0400

    Added Firebase Analytics Sample

---
+++ firebase-analytics/samples/FirebaseAnalyticsQuickstart/ImageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FirebaseAnalyticsQuickstart
+{
+    public class ImageInfo
+    {
+        public int Image { get; private set; }
+        public int Title { get; private set; }
+        public int Id { get; private set; }
+
+        /**
+         * Create a new ImageInfo.
+         */
+        public ImageInfo (int image, int title, int id)
+        {
+            Image = image;
+            Title = title;
+            Id = id;
+        }
+    }
+}
