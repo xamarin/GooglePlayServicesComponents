7e5db2442ded99a923131d6d3f31c089adcccefc
Redth <jondick@gmail.com>
2016-06-21 10:19:30 -0400

    Added Firebase Crash Reporting Sample

---
+++ firebase-crash/samples/FirebaseCrashReportingQuickstart/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Firebase.Crash;
+using Android.Util;
+
+namespace FirebaseCrashReportingQuickstart
+{
+    [Activity (Label = "Firebase Crash Reporting Quickstart", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+    public class MainActivity : AppCompatActivity
+    {
+        const string TAG = "MainActivity";
+
+        protected override void OnCreate (Bundle savedInstanceState)
+        {
+            base.OnCreate (savedInstanceState);
+
+            SetContentView (Resource.Layout.activity_main);
+
+            // Checkbox to indicate when to catch the thrown exception.
+            var catchCrashCheckBox = FindViewById<CheckBox> (Resource.Id.catchCrashCheckBox);
+
+            // Button that causes the NullPointerException to be thrown.
+            var crashButton = FindViewById<Button> (Resource.Id.crashButton);
+            crashButton.Click += (sender, e) => {
+                // Log that crash button was clicked. This version of Crash.log() will include the
+                // message in the crash report as well as show the message in logcat.
+                FirebaseCrash.Logcat (LogPriority.Info, TAG, "Crash button clicked");
+
+                // If catchCrashCheckBox is checked catch the exception and report is using
+                // Crash.report(). Otherwise throw the exception and let Firebase Crash automatically
+                // report the crash.
+                if (catchCrashCheckBox.Checked) {
+                    try {
+                        throw new Exception ();
+                    } catch (Exception ex) {
+                        // [START log_and_report]
+                        FirebaseCrash.Logcat (LogPriority.Error, TAG, "NPE caught");
+                        FirebaseCrash.Report (ex);
+                        // [END log_and_report]
+                        Java.Lang.Throwable.FromException (ex);
+                    }
+                } else {
+                    throw new Exception ("Intentionally Unhandled Exception");
+                }
+            };
+
+        // Log that the Activity was created. This version of Crash.log() will include the message
+        // in the crash report but will not be shown in logcat.
+        // [START log_event]
+        FirebaseCrash.Log ("Activity created");
+        // [END log_event]
+        }
+    }
+}
+
+
