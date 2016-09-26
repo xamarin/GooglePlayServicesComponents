d90783e2afc43f8918f41b79171ff50219cb47ca
Redth <jondick@gmail.com>
2016-09-26 12:15:51 -0400

    Add package name to consts to be used the same in all projects' attributes

--- GpsConsts.cs
+++ GpsConsts.cs
@@ -7,10 +7,12 @@ static class __GpsConsts {
     public const string Sha1sum = "ae24bde9c8f732f4d13b72e70802be8c97dcfddf";
     public const string Version = "9.4.0";
     public const string WearVersion = "1.4.0";
+    public const string PackageName = "Xamarin.GooglePlayServices";
 }
 
 static class __FbConsts
 {
+    public const string PackageName = __GpsConsts.PackageName;
     public const string Url = __GpsConsts.Url;
     public const string Sha1sum = __GpsConsts.Sha1sum;
     public const string Version = __GpsConsts.Version;
