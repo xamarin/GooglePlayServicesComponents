a6bb6c88954f4ff151677ef2b82a3f32b8e8734b
Redth <jondick@gmail.com>
2016-10-07 16:48:34 -0400

    Metadata and addition fixes for new version

--- firebase-auth/source/Additions/Additions.cs
+++ firebase-auth/source/Additions/Additions.cs
@@ -42,3 +42,54 @@ namespace Firebase.Auth
     }
 }
 
+namespace Firebase.Auth
+{
+    public partial class FirebaseUser
+    {
+        public System.Threading.Tasks.Task<IAuthResult> LinkWithCredentialAsync (AuthCredential credential)
+        {
+            return LinkWithCredential (credential).AsAsync<IAuthResult> ();
+        }
+
+        public System.Threading.Tasks.Task DeleteAsync ()
+        {
+            return Delete ().AsAsync ();
+        }
+
+        public System.Threading.Tasks.Task<GetTokenResult> GetTokenAsync (bool flag)
+        {
+            return GetToken (flag).AsAsync<GetTokenResult> ();
+        }
+
+        public System.Threading.Tasks.Task ReauthenticateAsync (AuthCredential credential)
+        {
+            return Reauthenticate (credential).AsAsync ();
+        }
+
+        public System.Threading.Tasks.Task ReloadAsync ()
+        {
+            return Reload ().AsAsync ();
+        }
+
+        public System.Threading.Tasks.Task<IAuthResult> UnlinkAsync (string value)
+        {
+            return Unlink (value).AsAsync<IAuthResult> ();
+        }
+
+        public System.Threading.Tasks.Task UpdateEmailAsync (string email)
+        {
+            return UpdateEmail (email).AsAsync ();
+        }
+
+        public System.Threading.Tasks.Task UpdatePasswordAsync (string password)
+        {
+            return UpdatePassword (password).AsAsync ();
+        }
+
+        public System.Threading.Tasks.Task UpdateProfileAsync (UserProfileChangeRequest userProfileChangeRequest)
+        {
+            return UpdateProfile (userProfileChangeRequest).AsAsync ();
+        }
+    }
+}
+
--- firebase-storage/source/Additions/Additions.cs
+++ firebase-storage/source/Additions/Additions.cs
@@ -69,5 +69,42 @@ namespace Firebase.Storage
         //     return GetDownloadUrl ().AsAsync<Android.Net.Uri> ();
         // }
     }
+
+    public partial class StorageException
+    {
+        static Delegate cb_getMessage;
+#pragma warning disable 0169
+        static Delegate GetGetMessageHandler ()
+        {
+            if (cb_getMessage == null)
+                cb_getMessage = JNINativeWrapper.CreateDelegate ((Func<IntPtr, IntPtr, IntPtr>) n_GetMessage);
+            return cb_getMessage;
+        }
+
+        static IntPtr n_GetMessage (IntPtr jnienv, IntPtr native__this)
+        {
+            global::Firebase.Storage.StorageException __this = global::Java.Lang.Object.GetObject<global::Firebase.Storage.StorageException> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+            return JNIEnv.NewString (__this.Message);
+        }
+#pragma warning restore 0169
+
+        static IntPtr id_getMessage;
+        public override unsafe string Message {
+            // Metadata.xml XPath method reference: path="/api/package[@name='com.google.firebase.storage']/class[@name='StorageException']/method[@name='getMessage' and count(parameter)=0]"
+            [Register ("getMessage", "()Ljava/lang/String;", "GetGetMessageHandler")]
+            get {
+                if (id_getMessage == IntPtr.Zero)
+                    id_getMessage = JNIEnv.GetMethodID (class_ref, "getMessage", "()Ljava/lang/String;");
+                try {
+
+                    if (GetType () == ThresholdType)
+                        return JNIEnv.GetString (JNIEnv.CallObjectMethod (((global::Java.Lang.Throwable) this).Handle, id_getMessage), JniHandleOwnership.TransferLocalRef);
+                    else
+                        return JNIEnv.GetString (JNIEnv.CallNonvirtualObjectMethod (((global::Java.Lang.Throwable) this).Handle, ThresholdClass, JNIEnv.GetMethodID (ThresholdClass, "getMessage", "()Ljava/lang/String;")), JniHandleOwnership.TransferLocalRef);
+                } finally {
+                }
+            }
+        }
+    }
 }
 
