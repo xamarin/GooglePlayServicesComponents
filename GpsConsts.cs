48314c53dd6d288c5c67d02fcf0647486ae20d78
Redth <jondick@gmail.com>
2016-11-23 19:07:59 -0500

    Fix incorrect download url and sha1sum

--- GpsConsts.cs
+++ GpsConsts.cs
@@ -3,8 +3,8 @@
 //      https://dl-ssl.google.com/android/repository/addon.xml
 
 static class __GpsConsts {
-    public const string Url  = "https://dl-ssl.google.com/android/repository/google_m2repository_r35.zip";
-    public const string Sha1sum = "209d1f3b4c5db966044436b793fb8fbd77a7eaf1";
+    public const string Url  = "https://dl-ssl.google.com/android/repository/google_m2repository_r36.zip";
+    public const string Sha1sum = "fa55e43b0175167da7f8cbedea35331cd448cf9b";
     public const string Version = "9.6.1";
     public const string WearVersion = "1.4.0";
     public const string PackageName = "Xamarin.GooglePlayServices";
