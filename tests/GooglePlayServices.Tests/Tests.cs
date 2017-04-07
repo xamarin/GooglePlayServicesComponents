1145b954fd5519f8c6dc12d48f3414cbaf71d7c5
Redth <jondick@gmail.com>
2017-04-07 09:34:12 -0400

    Added simple xunit test solution
    Going to start trying to track regressions specifically against any fixes made due to issues filed.

---
+++ tests/GooglePlayServices.Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.OS;
+using Android.Support.V4.App;
+using Xunit;
+using Xunit.Extensions;
+
+namespace GooglePlayServices.Tests
+{
+	public class Tests
+	{
+		[Fact]
+		public void GooglePlayServicesUtil_Exists ()
+		{
+			var versionCode = Android.Gms.Common.GooglePlayServicesUtil.GooglePlayServicesVersionCode;
+
+			Console.WriteLine("Google Play Services Version: {0}", versionCode);
+
+			Assert.True(versionCode > 0);
+		}
+
+		[Fact]
+		public void GoogleApiAvailability_Exists()
+		{
+			var versionCode = Android.Gms.Common.GoogleApiAvailability.GooglePlayServicesVersionCode;
+
+			Console.WriteLine("Google Play Services Version: {0}", versionCode);
+
+			Assert.True(versionCode > 0);
+		}
+	}
+}
