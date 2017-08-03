97cbe9f775d43cfb1cbd1897d6ad0d5b1f26c794
Robin Edbom <Robin.edbom@gmail.com>
2017-08-03 10:56:43 +0200

    BardecodeGraphic not scaling before Draw()
    After BarcodeCode is captured and analyzed, it does not scale to correct size before being drawn on the view.

--- vision/samples/VisionSample/BarcodeGraphic.cs
+++ vision/samples/VisionSample/BarcodeGraphic.cs
@@ -6,10 +6,10 @@ namespace VisionSample
 {
     class BarcodeGraphic : GraphicOverlay.Graphic 
     {
-        const float ID_TEXT_SIZE = 40.0f;
-        const float ID_Y_OFFSET = 50.0f;
-        const float ID_X_OFFSET = -50.0f;
-        const float BOX_STROKE_WIDTH = 5.0f;
+        const float IdTextSize = 40.0f;
+        const float IdYOffset = 50.0f;
+        const float IdXOffset = -50.0f;
+        const float BoxStrokeWidth = 5.0f;
 
         readonly Color[] COLOR_CHOICES = {
             Color.Blue,
@@ -40,12 +40,12 @@ namespace VisionSample
 
             mIdPaint = new Paint();
             mIdPaint.Color = selectedColor;
-            mIdPaint.TextSize = ID_TEXT_SIZE;
+            mIdPaint.TextSize = IdTextSize;
 
             mBoxPaint = new Paint();
             mBoxPaint.Color = selectedColor;
             mBoxPaint.SetStyle (Paint.Style.Stroke);
-            mBoxPaint.StrokeWidth = BOX_STROKE_WIDTH;
+            mBoxPaint.StrokeWidth = BoxStrokeWidth;
         }
 
         /**
@@ -70,11 +70,14 @@ namespace VisionSample
             if (barcode == null)
                 return;
 
-            var v = barcode.DisplayValue;
+            var rect = new Rect(barcode.BoundingBox);
+            rect.Left = (int) TranslateX(rect.Left);
+            rect.Top = (int) TranslateY(rect.Top);
+            rect.Right = (int) TranslateX(rect.Right);
+            rect.Bottom = (int) TranslateY(rect.Bottom);
+            canvas.DrawRect(rect,mBoxPaint);
 
-            //canvas.DrawText ("id: " + Id, x + ID_X_OFFSET, y + ID_Y_OFFSET, mIdPaint);
-
-            canvas.DrawRect (barcode.BoundingBox.Left, barcode.BoundingBox.Top, barcode.BoundingBox.Right, barcode.BoundingBox.Bottom, mBoxPaint);
+            canvas.DrawText (barcode.DisplayValue,rect.Left + IdXOffset,rect.Bottom + IdYOffset, mIdPaint);
         }
     }
 }
