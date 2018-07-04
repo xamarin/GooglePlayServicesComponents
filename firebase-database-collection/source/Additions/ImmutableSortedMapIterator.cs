416f9243b0b0b83db01abf6ffc5dcd9e2c4ccce0
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-07-04 08:29:31 +0200

    fixed NEW bindings projects namespace to package mappings (and some older ones too)

---
+++ firebase-database-collection/source/Additions/ImmutableSortedMapIterator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Java.Util;
+
+namespace Firebase.Database.Collection
+{
+    // Metadata.xml XPath class reference: path="/api/package[@name='com.google.firebase.database.collection']/class[@name='ImmutableSortedMapIterator']"
+    public partial class ImmutableSortedMapIterator
+    {
+        Java.Lang.Object IIterator.Next()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
