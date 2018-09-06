0c22c147fc534b592bee59ac9f97950bb5d1ebbd
adlair <adlair.cerecedo@xamarin.com>
2018-09-06 03:45:39 -0500

    In Progress.

---
+++ firebase-firestore/source/Additions/Additions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.OS;
+using Android.Runtime;
+using Java.Lang;
+
+namespace Com.Google.Firebase
+{
+    public partial class Timestamp : global::Java.Lang.Object, global::Android.OS.IParcelable, global::Java.Lang.IComparable
+    {
+        int Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
