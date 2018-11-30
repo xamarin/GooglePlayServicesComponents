4084265d21861a2afdc474e6cf9ce598fdb5eef1
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-11-30 19:55:29 +0100

    updates (firebase-iid) and cleanup

--- source/com.google.firebase/firebase-iid/Additions/Additions.cs
+++ source/com.google.firebase/firebase-iid/Additions/Additions.cs
@@ -5,9 +5,10 @@ namespace Firebase.Iid
 {
     public partial class FirebaseInstanceId 
 	{
-        public System.Threading.Tasks.Task<IInstanceIdResult> GetInstanceIdAsync ()
-        {
-            return GetInstanceId ().AsAsync<IInstanceIdResult> ();
-        }
+        // IInstanceIdResult - deprecated
+        //public System.Threading.Tasks.Task<IInstanceIdResult> GetInstanceIdAsync ()
+        //{
+        //    return GetInstanceId ().AsAsync<IInstanceIdResult> ();
+        //}
     }
 }
