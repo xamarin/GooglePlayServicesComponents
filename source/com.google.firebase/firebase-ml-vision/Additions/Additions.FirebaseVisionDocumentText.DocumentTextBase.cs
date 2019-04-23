46dc757284f429ba3d6d30710da1f28a1c6b03c2
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-04-23 13:01:55 +0200

    firebase-ml-vision updates

---
+++ source/com.google.firebase/firebase-ml-vision/Additions/Additions.FirebaseVisionDocumentText.DocumentTextBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.Runtime;
+
+namespace Firebase.ML.Vision.Document
+{
+    // Metadata.xml XPath class reference: path="/api/package[@name='com.google.firebase.ml.vision.document']/class[@name='FirebaseVisionDocumentText']"
+    //[global::Android.Runtime.Register ("com/google/firebase/ml/vision/document/FirebaseVisionDocumentText", DoNotGenerateAcw=true)]
+    public partial class FirebaseVisionDocumentText //: global::Java.Lang.Object
+    {
+        // Metadata.xml XPath class reference: path="/api/package[@name='com.google.firebase.ml.vision.document']/class[@name='FirebaseVisionDocumentText.DocumentTextBase']"
+        // [global::Android.Runtime.Register("com/google/firebase/ml/vision/document/FirebaseVisionDocumentText$DocumentTextBase", DoNotGenerateAcw = true)]
+        public partial class DocumentTextBase // : global::Java.Lang.Object
+        {
+			static Delegate cb_FirebaseVisionDocumentText_DocumentTextBase_getRecognizedLanguages;
+#pragma warning disable 0169
+			static Delegate GetGetRecognizedLanguagesHandler ()
+			{
+				if (cb_FirebaseVisionDocumentText_DocumentTextBase_getRecognizedLanguages == null)
+					cb_FirebaseVisionDocumentText_DocumentTextBase_getRecognizedLanguages = JNINativeWrapper.CreateDelegate ((Func<IntPtr, IntPtr, IntPtr>) n_GetRecognizedLanguages);
+				return cb_FirebaseVisionDocumentText_DocumentTextBase_getRecognizedLanguages;
+			}
+
+			static IntPtr n_GetRecognizedLanguages (IntPtr jnienv, IntPtr native__this)
+			{
+				global::Firebase.ML.Vision.Document.FirebaseVisionDocumentText.DocumentTextBase __this = global::Java.Lang.Object.GetObject<global::Firebase.ML.Vision.Document.FirebaseVisionDocumentText.DocumentTextBase> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+				return global::Android.Runtime.JavaList<global::Firebase.ML.Vision.Text.RecognizedLanguage>.ToLocalJniHandle (__this.RecognizedLanguages);
+			}
+#pragma warning restore 0169
+
+			public virtual unsafe global::System.Collections.Generic.IList<global::Firebase.ML.Vision.Text.RecognizedLanguage> RecognizedLanguages {
+				// Metadata.xml XPath method reference: path="/api/package[@name='com.google.firebase.ml.vision.document']/class[@name='FirebaseVisionDocumentText.DocumentTextBase']/method[@name='getRecognizedLanguages' and count(parameter)=0]"
+				[Register ("getRecognizedLanguages", "()Ljava/util/List;", "GetGetRecognizedLanguagesHandler")]
+				get {
+					const string __id = "getRecognizedLanguages.()Ljava/util/List;";
+					try {
+						var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod (__id, this, null);
+						return global::Android.Runtime.JavaList<global::Firebase.ML.Vision.Text.RecognizedLanguage>.FromJniHandle (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+					} finally {
+					}
+				}
+			}
+        }
+    }
+
+}
---
+++ source/com.google.firebase/firebase-ml-vision/Additions/Additions.FirebaseVisionText.TextBlock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.Runtime;
+
+namespace Firebase.ML.Vision.Text
+{
+
+    // Metadata.xml XPath class reference: path="/api/package[@name='com.google.firebase.ml.vision.text']/class[@name='FirebaseVisionText']"
+    // [global::Android.Runtime.Register ("com/google/firebase/ml/vision/text/FirebaseVisionText", DoNotGenerateAcw=true)]
+    public partial class FirebaseVisionText // : global::Java.Lang.Object
+    {
+
+        // Metadata.xml XPath class reference: path="/api/package[@name='com.google.firebase.ml.vision.text']/class[@name='FirebaseVisionText.Element']"
+        // [global::Android.Runtime.Register("com/google/firebase/ml/vision/text/FirebaseVisionText$Element", DoNotGenerateAcw = true)]
+        public partial class TextBlock // : global::Java.Lang.Object
+        {
+			static Delegate cb_FirebaseVisionText_TextBlock_getRecognizedLanguages;
+#pragma warning disable 0169
+			static Delegate GetGetRecognizedLanguagesHandler ()
+			{
+				if (cb_FirebaseVisionText_TextBlock_getRecognizedLanguages == null)
+					cb_FirebaseVisionText_TextBlock_getRecognizedLanguages = JNINativeWrapper.CreateDelegate ((Func<IntPtr, IntPtr, IntPtr>) n_GetRecognizedLanguages);
+				return cb_FirebaseVisionText_TextBlock_getRecognizedLanguages;
+			}
+
+			static IntPtr n_GetRecognizedLanguages (IntPtr jnienv, IntPtr native__this)
+			{
+				global::Firebase.ML.Vision.Text.FirebaseVisionText.TextBlock __this = global::Java.Lang.Object.GetObject<global::Firebase.ML.Vision.Text.FirebaseVisionText.TextBlock> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+				return global::Android.Runtime.JavaList.ToLocalJniHandle (__this.RecognizedLanguages);
+			}
+#pragma warning restore 0169
+
+			public virtual unsafe global::System.Collections.IList RecognizedLanguages {
+				// Metadata.xml XPath method reference: path="/api/package[@name='com.google.firebase.ml.vision.text']/class[@name='FirebaseVisionText.TextBlock']/method[@name='getRecognizedLanguages' and count(parameter)=0]"
+				[Register ("getRecognizedLanguages", "()Ljava/util/List;", "GetGetRecognizedLanguagesHandler")]
+				get {
+					const string __id = "getRecognizedLanguages.()Ljava/util/List;";
+					try {
+						var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod (__id, this, null);
+						return global::Android.Runtime.JavaList.FromJniHandle (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+					} finally {
+					}
+				}
+			}
+        }
+    }
+}
