f638263983835a2c175a5ea3b21d71dac2155bf9
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-05-28 07:51:46 +0200

    unit tests added for Vision (java.lang.NoClassDefFoundError)

---
+++ tests/GooglePlayServices.Tests/Tests_Vision.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.OS;
+using Android.Support.V4.App;
+using Xunit;
+
+namespace GooglePlayServices.Tests
+{
+	public class Tests_Vision
+	{
+		[Fact]
+		public void BarcodeDetector_Builder()
+		{
+			var detector = new BarcodeDetector.Builder (Application.Context).Build ();
+
+
+			Assert.True(detector != null);
+		}
+
+		[Fact]
+		public void FaceDetector_Builder()
+		{
+			var stillFaceDetector = new FaceDetector.Builder(Android.App.Application.Context)
+			.SetTrackingEnabled(true)
+			.SetMode(FaceDetectionMode.Accurate)
+			.SetLandmarkType(LandmarkDetectionType.None)
+			.Build();
+
+			Assert.True(stillFaceDetector != null);
+		}
+	}
+}
