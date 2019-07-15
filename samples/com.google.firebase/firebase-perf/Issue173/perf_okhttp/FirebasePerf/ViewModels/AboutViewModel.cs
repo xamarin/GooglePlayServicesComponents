2fd67cf8f281fee7da6d54afa4775e9016835d8c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-07-15 23:03:15 +0200

    firebase-perf samples path shortening

---
+++ samples/com.google.firebase/firebase-perf/Issue173/perf_okhttp/FirebasePerf/ViewModels/AboutViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+using Square.OkHttp3;
+using Xamarin.Forms;
+
+namespace FirebasePerf.ViewModels
+{
+	public class AboutViewModel : BaseViewModel
+	{
+		public AboutViewModel()
+		{
+			Title = "About";
+
+			OpenWebCommand = new Command(MakeHttpCall);
+		}
+
+		public ICommand OpenWebCommand { get; }
+
+		private async void MakeHttpCall()
+		{
+#if __ANDROID__
+			var client = new OkHttpClient();
+			var requestBody = RequestBody.Create(
+				MediaType.Parse("application/json"),
+				"{\"title\":\"foo\",\"body\": \"bar\", \"userId\":\"1\", \"id\":\"1\"}");
+			var request = new Request.Builder()
+				.Url("https://jsonplaceholder.typicode.com/posts")
+				.Post(requestBody)
+				.Build();
+
+			var response = await client.NewCall(request).ExecuteAsync();
+#endif
+		}
+	}
+}
