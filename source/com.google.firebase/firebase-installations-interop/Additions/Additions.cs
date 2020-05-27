c537161f263cb4ce0ea8a8b64c11c621a93826ae
moljac <mcvjetko@holisticware.net>
2020-05-27 05:37:05 +0200

    Com namespaces fixed

---
+++ source/com.google.firebase/firebase-installations-interop/Additions/Additions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Android.Gms.Extensions;
+
+namespace Firebase.Iid 
+{
+    public partial class FirebaseInstanceId 
+	{
+        // IInstanceIdResult - deprecated
+        //public System.Threading.Tasks.Task<IInstanceIdResult> GetInstanceIdAsync ()
+        //{
+        //    return GetInstanceId ().AsAsync<IInstanceIdResult> ();
+        //}
+    }
+}
