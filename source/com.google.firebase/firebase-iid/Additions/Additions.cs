3696b4ed441c8396072119ebdf99496bc9ae4cfd
moljac <mcvjetko@holisticware.net>
2020-05-28 14:50:01 +0200

    Additions cleanup for firebase-iid

--- source/com.google.firebase/firebase-iid/Additions/Additions.cs
+++ source/com.google.firebase/firebase-iid/Additions/Additions.cs
@@ -3,12 +3,4 @@ using Android.Gms.Extensions;
 
 namespace Firebase.Iid 
 {
-    public partial class FirebaseInstanceId 
-	{
-        // IInstanceIdResult - deprecated
-        //public System.Threading.Tasks.Task<IInstanceIdResult> GetInstanceIdAsync ()
-        //{
-        //    return GetInstanceId ().AsAsync<IInstanceIdResult> ();
-        //}
-    }
 }
--- source/com.google.firebase/firebase-installations-interop/Additions/Additions.cs
+++ source/com.google.firebase/firebase-installations-interop/Additions/Additions.cs
@@ -1,14 +1,3 @@
 ﻿using System;
 using Android.Gms.Extensions;
 
-namespace Firebase.Iid 
-{
-    public partial class FirebaseInstanceId 
-	{
-        // IInstanceIdResult - deprecated
-        //public System.Threading.Tasks.Task<IInstanceIdResult> GetInstanceIdAsync ()
-        //{
-        //    return GetInstanceId ().AsAsync<IInstanceIdResult> ();
-        //}
-    }
-}
