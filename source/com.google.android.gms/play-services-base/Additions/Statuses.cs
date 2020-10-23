34bfe44d53f49a9cbeaef53b8b9b953479b60aad
moljac <mcvjetko@holisticware.net>
2020-10-23 12:42:13 +0200

    fixes according to review

---
+++ source/com.google.android.gms/play-services-base/Additions/Statuses.cs
@@ -0,0 +1,9 @@
+namespace Android.Gms.Common.Apis 
+{
+
+	// Metadata.xml XPath class reference: path="/api/package[@name='com.google.android.gms.common.api']/class[@name='Status']"
+	//[global::Android.Runtime.Register ("com/google/android/gms/common/api/Status", DoNotGenerateAcw=true)]
+	//public sealed partial class Statuses : Android.Gms.Common.Apis.IResult
+	//{
+	//}
+}
