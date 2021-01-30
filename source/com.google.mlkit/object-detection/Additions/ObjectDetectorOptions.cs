b29ca379a316bd8f43433f133c3dd340972018fa
moljac <mcvjetko@holisticware.net>
2021-01-30 10:16:46 +0100

    MLKit + Play.Core moved back to the repo

---
+++ source/com.google.mlkit/object-detection/Additions/ObjectDetectorOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Xamarin.GoogleFirebase.MLKit.ObjectDetection.Additions
+{
+    public class ObjectDetectorOptions
+    {
+        public ObjectDetectorOptions()
+        {
+        }
+    }
+}
