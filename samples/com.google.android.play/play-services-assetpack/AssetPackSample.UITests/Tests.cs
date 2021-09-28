3bddeb67b236cf2baf6fc2ee5f6245a8d0bc721f
Dean Ellis <dellis1972@googlemail.com>
2021-09-28 10:59:39 +0100

    Initial Commit of Play Core Extensions

---
+++ samples/com.google.android.play/play-services-assetpack/AssetPackSample.UITests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace AssetPackSample.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.StartApp();
+		}
+
+		[Test]
+		public void AppDidLaunch()
+		{
+			app.WaitForElement(c => c.Id("content"));
+			app.Screenshot("App launched");
+		}
+	}
+}
