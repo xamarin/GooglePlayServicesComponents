85cf10a9cd004dd851452d30377ea1a68b160b4d
moljac <mcvjetko@holisticware.net>
2021-11-25 15:41:00 +0100

    protobuf metadata fixes

--- source/com.google.protobuf/protobuf-lite/Additions/LazyStringArrayList.cs
+++ source/com.google.protobuf/protobuf-lite/Additions/LazyStringArrayList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.Runtime;
+using Java.Interop;
 
 namespace Xamarin.Protobuf.Lite
 {
@@ -43,5 +44,78 @@ namespace Xamarin.Protobuf.Lite
             {
             }
         }
+
+
+		static Delegate cb_addAll_Ljava_util_Collection_;
+#pragma warning disable 0169
+		static Delegate GetAddAll_Ljava_util_Collection_Handler ()
+		{
+			if (cb_addAll_Ljava_util_Collection_ == null)
+				cb_addAll_Ljava_util_Collection_ = JNINativeWrapper.CreateDelegate ((_JniMarshal_PPL_Z) n_AddAll_Ljava_util_Collection_);
+			return cb_addAll_Ljava_util_Collection_;
+		}
+
+		static bool n_AddAll_Ljava_util_Collection_ (IntPtr jnienv, IntPtr native__this, IntPtr native_c)
+		{
+			var __this = global::Java.Lang.Object.GetObject<global::Xamarin.Protobuf.Lite.LazyStringArrayList> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+			var c = global::Android.Runtime.JavaCollection<string>.FromJniHandle (native_c, JniHandleOwnership.DoNotTransfer);
+			bool __ret = __this.AddAll (c);
+			return __ret;
+		}
+#pragma warning restore 0169
+
+		// Metadata.xml XPath method reference: path="/api/package[@name='com.google.protobuf']/class[@name='LazyStringArrayList']/method[@name='addAll' and count(parameter)=1 and parameter[1][@type='java.util.Collection&lt;? extends java.lang.String&gt;']]"
+		[Register ("addAll", "(Ljava/util/Collection;)Z", "GetAddAll_Ljava_util_Collection_Handler")]
+		public unsafe bool AddAll (global::System.Collections.Generic.ICollection<string> c)
+		{
+			const string __id = "addAll.(Ljava/util/Collection;)Z";
+			IntPtr native_c = global::Android.Runtime.JavaCollection<string>.ToLocalJniHandle (c);
+			try {
+				JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+				__args [0] = new JniArgumentValue (native_c);
+				var __rm = _members.InstanceMethods.InvokeVirtualBooleanMethod (__id, this, __args);
+				return __rm;
+			} finally {
+				JNIEnv.DeleteLocalRef (native_c);
+				global::System.GC.KeepAlive (c);
+			}
+		}
+
+
+		static Delegate cb_addAll_ILjava_util_Collection_;
+#pragma warning disable 0169
+		static Delegate GetAddAll_ILjava_util_Collection_Handler ()
+		{
+			if (cb_addAll_ILjava_util_Collection_ == null)
+				cb_addAll_ILjava_util_Collection_ = JNINativeWrapper.CreateDelegate ((_JniMarshal_PPIL_Z) n_AddAll_ILjava_util_Collection_);
+			return cb_addAll_ILjava_util_Collection_;
+		}
+
+		static bool n_AddAll_ILjava_util_Collection_ (IntPtr jnienv, IntPtr native__this, int index, IntPtr native_c)
+		{
+			var __this = global::Java.Lang.Object.GetObject<global::Xamarin.Protobuf.Lite.LazyStringArrayList> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+			var c = global::Android.Runtime.JavaCollection<string>.FromJniHandle (native_c, JniHandleOwnership.DoNotTransfer);
+			bool __ret = __this.AddAll (index, c);
+			return __ret;
+		}
+#pragma warning restore 0169
+
+		// Metadata.xml XPath method reference: path="/api/package[@name='com.google.protobuf']/class[@name='LazyStringArrayList']/method[@name='addAll' and count(parameter)=2 and parameter[1][@type='int'] and parameter[2][@type='java.util.Collection&lt;? extends java.lang.String&gt;']]"
+		[Register ("addAll", "(ILjava/util/Collection;)Z", "GetAddAll_ILjava_util_Collection_Handler")]
+		public unsafe bool AddAll (int index, global::System.Collections.Generic.ICollection<string> c)
+		{
+			const string __id = "addAll.(ILjava/util/Collection;)Z";
+			IntPtr native_c = global::Android.Runtime.JavaCollection<string>.ToLocalJniHandle (c);
+			try {
+				JniArgumentValue* __args = stackalloc JniArgumentValue [2];
+				__args [0] = new JniArgumentValue (index);
+				__args [1] = new JniArgumentValue (native_c);
+				var __rm = _members.InstanceMethods.InvokeVirtualBooleanMethod (__id, this, __args);
+				return __rm;
+			} finally {
+				JNIEnv.DeleteLocalRef (native_c);
+				global::System.GC.KeepAlive (c);
+			}
+		}
     }
 }
