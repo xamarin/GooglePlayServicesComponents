a6d7105eb6636beb09210ab76c01edaf69d37d66
moljac <mcvjetko@holisticware.net>
2021-12-17 15:58:09 +0100

    grpc metadata

---
+++ source/io.grpc/grpc-api/Additions/Xamarin.Grpc.CodecGzip.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+using Android.Runtime;
+
+namespace Xamarin.Grpc 
+{
+
+	// Metadata.xml XPath class reference: path="/api/package[@name='io.grpc']/class[@name='Codec.Gzip']"
+	//[global::Android.Runtime.Register ("io/grpc/Codec$Gzip", DoNotGenerateAcw=true)]
+	public sealed partial class CodecGzip 
+    {
+ 		public unsafe string MessageEncodingCompressor
+        {
+			// Metadata.xml XPath method reference: path="/api/package[@name='io.grpc']/class[@name='Codec.Gzip']/method[@name='getMessageEncoding' and count(parameter)=0]"
+			[Register ("getMessageEncoding", "()Ljava/lang/String;", "")]
+			get {
+				const string __id = "getMessageEncoding.()Ljava/lang/String;";
+				try {
+					var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
+					return JNIEnv.GetString (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+				} finally {
+				}
+			}
+		}
+
+ 		public unsafe string MessageEncodingDecompressor
+        {
+			// Metadata.xml XPath method reference: path="/api/package[@name='io.grpc']/class[@name='Codec.Gzip']/method[@name='getMessageEncoding' and count(parameter)=0]"
+			[Register ("getMessageEncoding", "()Ljava/lang/String;", "")]
+			get {
+				const string __id = "getMessageEncoding.()Ljava/lang/String;";
+				try {
+					var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
+					return JNIEnv.GetString (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+				} finally {
+				}
+			}
+		}
+   }
+}
