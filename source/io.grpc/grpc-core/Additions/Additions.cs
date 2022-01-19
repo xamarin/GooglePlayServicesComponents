b60be920e3b68191564aa8cb831d3d693d7e2412
moljac <mcvjetko@holisticware.net>
2022-01-19 16:35:01 +0100

    metadata fixes to surface more clases

--- source/io.grpc/grpc-core/Additions/Additions.cs
+++ source/io.grpc/grpc-core/Additions/Additions.cs
@@ -2,3 +2,29 @@
 using Android.Views;
 using Android.Widget;
 using Android.Graphics;
+
+
+namespace Xamarin.Grpc.Core.InProcess 
+{
+	public sealed partial class InProcessChannelBuilder 
+    {
+		// public override unsafe global::Java.Lang.Object Intercept (params global::Xamarin.Grpc.IClientInterceptor[] p0)
+		// {
+		// 	const string __id = "intercept.([Lio/grpc/ClientInterceptor;)Lio/grpc/inprocess/InProcessChannelBuilder;";
+		// 	IntPtr native_p0 = JNIEnv.NewArray (p0);
+		// 	try {
+		// 		JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+		// 		__args [0] = new JniArgumentValue (native_p0);
+		// 		var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, __args);
+		// 		return (global::Java.Lang.Object) global::Java.Lang.Object.GetObject<global::Java.Lang.Object> (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+		// 	} finally {
+		// 		if (p0 != null) {
+		// 			JNIEnv.CopyArray (native_p0, p0);
+		// 			JNIEnv.DeleteLocalRef (native_p0);
+		// 		}
+		// 		global::System.GC.KeepAlive (p0);
+		// 	}
+		// }
+        
+    }
+}
