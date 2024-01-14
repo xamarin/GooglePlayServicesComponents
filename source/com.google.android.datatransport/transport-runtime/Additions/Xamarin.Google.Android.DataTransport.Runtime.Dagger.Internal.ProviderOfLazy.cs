4a695151835ac9eeb957aaf1e73660ad4b3dd7a0
moljac <mcvjetko@holisticware.net>
2024-01-14 14:35:17 +0100

    Update Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal.ProviderOfLazy.cs

--- source/com.google.android.datatransport/transport-runtime/Additions/Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal.ProviderOfLazy.cs
+++ source/com.google.android.datatransport/transport-runtime/Additions/Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal.ProviderOfLazy.cs
@@ -13,15 +13,15 @@ namespace Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal
     {
 
  		// Metadata.xml XPath method reference: path="/api/package[@name='com.google.android.datatransport.runtime.dagger.internal']/class[@name='ProviderOfLazy']/method[@name='get' and count(parameter)=0]"
-		[Register ("get", "()Lcom/google/android/datatransport/runtime/dagger/Lazy;", "")]
-		public unsafe global::Java.Lang.Object Get ()
-		{
-			const string __id = "get.()Lcom/google/android/datatransport/runtime/dagger/Lazy;";
-			try {
-				var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
-				return (Java.Lang.Object) global::Java.Lang.Object.GetObject<global::Xamarin.Google.Android.DataTransport.Runtime.Dagger.ILazy> (__rm.Handle, JniHandleOwnership.TransferLocalRef);
-			} finally {
-			}
-		}
+		// [Register ("get", "()Lcom/google/android/datatransport/runtime/dagger/Lazy;", "")]
+		// public unsafe global::Java.Lang.Object Get ()
+		// {
+		// 	const string __id = "get.()Lcom/google/android/datatransport/runtime/dagger/Lazy;";
+		// 	try {
+		// 		var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
+		// 		return (Java.Lang.Object) global::Java.Lang.Object.GetObject<global::Xamarin.Google.Android.DataTransport.Runtime.Dagger.ILazy> (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+		// 	} finally {
+		// 	}
+		// }
    }
 }
