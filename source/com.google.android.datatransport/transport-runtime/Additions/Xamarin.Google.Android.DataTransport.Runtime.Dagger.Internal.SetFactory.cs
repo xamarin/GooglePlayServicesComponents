e4028825c7aad391757b528545cf6e3fdbee5e48
moljac <mcvjetko@holisticware.net>
2024-01-18 12:42:42 +0100

    metadata fixes firebase-config and datatransport

--- source/com.google.android.datatransport/transport-runtime/Additions/Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal.SetFactory.cs
+++ source/com.google.android.datatransport/transport-runtime/Additions/Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal.SetFactory.cs
@@ -11,15 +11,15 @@ namespace Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal
 	public sealed partial class SetFactory //: global::Java.Lang.Object, global::Xamarin.Google.Android.DataTransport.Runtime.Dagger.Internal.IFactory 
     {
 		// Metadata.xml XPath method reference: path="/api/package[@name='com.google.android.datatransport.runtime.dagger.internal']/class[@name='SetFactory']/method[@name='get' and count(parameter)=0]"
-		[Register ("get", "()Ljava/util/Set;", "")]
-		public unsafe global::Java.Lang.Object Get ()
-		{
-			const string __id = "get.()Ljava/util/Set;";
-			try {
-				var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
-				return (Java.Lang.Object) global::Android.Runtime.JavaSet.FromJniHandle (__rm.Handle, JniHandleOwnership.TransferLocalRef);
-			} finally {
-			}
-		}
+		// [Register ("get", "()Ljava/util/Set;", "")]
+		// public unsafe global::Java.Lang.Object Get ()
+		// {
+		// 	const string __id = "get.()Ljava/util/Set;";
+		// 	try {
+		// 		var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod (__id, this, null);
+		// 		return (Java.Lang.Object) global::Android.Runtime.JavaSet.FromJniHandle (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+		// 	} finally {
+		// 	}
+		// }
     }
 }
