9730e2d8b3799df73883b095e10485b75f05abcd
Dean Ellis <dellis1972@googlemail.com>
2024-03-05 15:46:47 +0000

    Add missing type

---
+++ source/com.google.android.play/feature.delivery.extensions/extensions-aar/src/main/java/xamarin/google/android/play/core/splitInstall/SplitInstallStateUpdatedListenerWrapper.java
@@ -0,0 +1,40 @@
+package xamarin.google.android.play.core.splitInstall;
+
+import com.google.android.play.core.splitinstall.SplitInstallManager;
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory;
+import com.google.android.play.core.splitinstall.SplitInstallRequest;
+import com.google.android.play.core.splitinstall.SplitInstallSessionState;
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
+
+public class SplitInstallStateUpdatedListenerWrapper {
+    private SplitInstallStateUpdatedListener splitInstallStateUpdatedListener = state -> {
+        onStateUpdate (state);
+    };
+
+    void onStateUpdate(SplitInstallSessionState state)
+    {
+        if (stateUpdateListener != null)
+            stateUpdateListener.OnStateUpdate(state);
+    }
+
+    public SplitInstallStateUpdatedListener GetListener()
+    {
+        return splitInstallStateUpdatedListener;
+    }
+
+    public interface StateUpdateListener {
+        /**
+         * Called when an the State is Updated
+         *
+         * @param state The Install Session State.
+         */
+        abstract void OnStateUpdate (SplitInstallSessionState state);
+    }
+
+    private StateUpdateListener stateUpdateListener = null;
+
+    public void setStateUpdateListener (StateUpdateListener listener)
+    {
+        stateUpdateListener = listener;
+    }
+}
