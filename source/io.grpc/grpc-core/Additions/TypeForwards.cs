d59f735549117a8733c2f1ddeeec7a57194cd76e
moljac <mcvjetko@holisticware.net>
2024-04-05 09:14:56 +0200

    TypeForwards added for moved types

---
+++ source/io.grpc/grpc-core/Additions/TypeForwards.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.AdvancedTlsX509KeyManager))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.AdvancedTlsX509TrustManager))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.CertificateUtils))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.ForwardingClientStreamTracer))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.ForwardingLoadBalancer))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.ForwardingLoadBalancerHelper))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.ForwardingSubchannel))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.GracefulSwitchLoadBalancer))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.MutableHandlerRegistry))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.OutlierDetectionLoadBalancer))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.OutlierDetectionLoadBalancerProvider))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.RoundRobinLoadBalancer))]
+[assembly: TypeForwardedTo (typeof (Xamarin.Grpc.Core.Util.TransmitStatusRuntimeExceptionInterceptor))]
