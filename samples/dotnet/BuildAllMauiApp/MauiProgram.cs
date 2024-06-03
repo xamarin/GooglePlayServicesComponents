bca8b052e6ccfa82221446a0b372422a79146347
moljac <mcvjetko@holisticware.net>
2024-06-03 11:29:52 +0200

    MAUI App sample renewed

--- samples/dotnet/BuildAllMauiApp/MauiProgram.cs
+++ samples/dotnet/BuildAllMauiApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace BuildAllMauiApp;
+﻿using Microsoft.Extensions.Logging;
+
+namespace BuildAllMauiApp;
 
 public static class MauiProgram
 {
@@ -12,6 +14,10 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
 
 		return builder.Build();
 	}
